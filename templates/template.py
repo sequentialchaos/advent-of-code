f8bfe67faa2a00699bb2360da51ce7b11d3cc998
Winston Smith <wsbraves@gmail.com>
2020-12-05 21:24:15 -0500

    add python template

---
+++ templates/template.py
@@ -0,0 +1,21 @@
+def main():
+  s = read_input()
+  print(f'part 1: {part_1(s)}')
+  print(f'part 2: {part_2(s)}')
+
+def part_1(s):
+  return 0
+
+def part_2(s):
+  return 0
+
+#####################################################################
+
+def read_input():
+  with open('../inputs/05.txt') as f:
+    return f.read().strip()
+
+#####################################################################
+
+if __name__ == "__main__":
+  main()
