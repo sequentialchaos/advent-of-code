3a1b34d596ac6fb66f3aaea86be8b87eaa7ca2ef
Winston Smith <wsbraves@gmail.com>
2021-12-03 11:12:48 -0500

    update table

--- make_markdown_table.py
+++ make_markdown_table.py
@@ -9,77 +9,86 @@ def main():
 
 
 def read_readme():
-    readme_str = ''
-    with open('README.md', 'r') as f:
+    readme_str = ""
+    with open("README.md", "r") as f:
         for line in f.readlines():
-            if 'day ↓ year →' in line:
+            if "day ↓ year →" in line:
                 break
             readme_str += line
     return readme_str
 
 
 def write_to_readme(readme_str):
-    with open('README.md', 'w') as f:
+    with open("README.md", "w") as f:
         f.write(readme_str)
 
 
 def make_table(d):
-    table = ''
-    table += '|day ↓ year → |'
-    for year in range(2015, 2020+1):
-        table += str(year) + '|'
-    table += '\n|:--------------:|'
-    for year in range(2015, 2020+1):
-        table += ':-------:|'
-    table += '\n'
-    for day in range(1, 25+1):
+    table = ""
+    table += "|day ↓ year → |"
+    last_year = 2021
+    for year in range(2015, last_year + 1):
+        table += str(year) + "|"
+    table += "\n|:--------------:|"
+    for year in range(2015, last_year + 1):
+        table += ":-------:|"
+    table += "\n"
+    for day in range(1, 25 + 1):
         day_str = str(day)
-        table += f'|{day}|'
-        for year in range(2015, 2020+1):
+        table += f"|{day}|"
+        for year in range(2015, last_year + 1):
             year_str = str(year)
             if year_str in d:
                 if day_str in d[year_str]:
-                    languages = d[year_str][day_str]['languages']
-                    urls = d[year_str][day_str]['urls']
+                    languages = d[year_str][day_str]["languages"]
+                    urls = d[year_str][day_str]["urls"]
                     for i in range(len(languages)):
-                        table += f'[{languages[i]}]({urls[i]})'
+                        extension = language_extension(languages[i])
+                        table += f"[{extension}]({urls[i]})"
                         if i < len(languages) - 1:
-                            table += ','
-            table += '|'
-        table += '\n'
+                            table += ","
+            table += "|"
+        table += "\n"
     return table
 
 
 def make_dict():
     d = {}
-    base_url = 'https://www.github.com/sequentialchaos/advent-of-code/tree/master/'
-    with open('completed.csv') as csv_completed:
-        csv_reader = csv.reader(csv_completed, delimiter=',')
-        line_count = 0
+    base_url = "https://www.github.com/sequentialchaos/advent-of-code/tree/master/"
+    with open("completed.csv") as csv_completed:
+        csv_reader = csv.reader(csv_completed, delimiter=",")
         for i, row in enumerate(csv_reader):
             if i == 0:
                 continue
             print(row)
-            year, day, languages = row[0], row[1], row[2].split(';')
+            year, day, languages = row[0], row[1], row[2].split(";")
             d.setdefault(year, {})
             d[year][day] = {}
-            d[year][day]['languages'] = languages
+            d[year][day]["languages"] = languages
             urls = []
             for language in languages:
-                urls.append(f'{base_url}{year}/{language}/' +
-                            url_end(language, day))
-            d[year][day]['urls'] = urls
+                urls.append(f"{base_url}{year}/{language}/" + url_end(language, day))
+            d[year][day]["urls"] = urls
     return d
 
 
 def url_end(language, day):
-    if language == 'javascript':
-        return f'day{day.zfill(2)}/solution.js'
-    elif language == 'python':
-        return f'day{day.zfill(2)}.py'
-    elif language == 'haskell':
-        return f'Day{day.zfill(2)}.hs'
+    if language == "javascript":
+        return f"day{day.zfill(2)}/solution.js"
+    elif language == "python":
+        return f"day{day.zfill(2)}.py"
+    elif language == "haskell":
+        return f"Day{day.zfill(2)}.hs"
 
 
-if __name__ == '__main__':
+def language_extension(language):
+    if language == "javascript":
+        return "js"
+    elif language == "python":
+        return "py"
+    elif language == "haskell":
+        return "hs"
+
+
+if __name__ == "__main__":
     main()
